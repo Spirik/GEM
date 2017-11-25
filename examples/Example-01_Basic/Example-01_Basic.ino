d4e0595c60a4a77fc3de6d0935095dbbfdbbbdbd
Alexander Spiridonov <alex.spiridonov@castle.co>
2017-11-25 15:12:19 +0400

    Example-01_Basic supplied

---
+++ examples/Example-01_Basic/Example-01_Basic.ino
@@ -0,0 +1,123 @@
+/*
+  Basic menu example using GEM library.
+
+  Simple one page menu with one editable menu item associated with int variable, one with boolean variable,
+  and a button, pressing of which will result in int variable value printed to Serial monitor if boolean variable is set to true.
+
+  KeyDetector library is used to detect push-buttons presses.
+  
+  Additional info (including the breadboard view) available on GitHub:
+  https://github.com/Spirik/GEM
+  
+  This example code is in the public domain.
+*/
+
+#include <GEM.h>
+#include <KeyDetector.h>
+
+// Pins the buttons are connected to
+const byte downPin = 2;
+const byte leftPin = 3;
+const byte rightPin = 4;
+const byte upPin = 5;
+const byte cancelPin = 6;
+const byte okPin = 7;
+
+// Array of Key objects that will link GEM key identifiers with dedicated pins
+Key keys[] = {{GEM_KEY_UP, upPin}, {GEM_KEY_RIGHT, rightPin}, {GEM_KEY_DOWN, downPin}, {GEM_KEY_LEFT, leftPin}, {GEM_KEY_CANCEL, cancelPin}, {GEM_KEY_OK, okPin}};
+
+// Create KeyDetector object
+KeyDetector myKeyDetector(keys, sizeof(keys)/sizeof(Key));
+// To account for switch bounce effect of the buttons (if occur) you may whant to specify debounceDelay
+// as the second argument to KeyDetector constructor:
+// KeyDetector myKeyDetector(keys, sizeof(keys)/sizeof(Key), 10);
+
+// Constants for the pins SparkFun Graphic LCD Serial Backpack is connected to and SoftwareSerial object
+const byte rxPin = 8;
+const byte txPin = 9;
+SoftwareSerial serialLCD(rxPin, txPin);
+
+// Create an instance of the GLCD class. This instance is used to call all the subsequent GLCD functions
+// (internally from GEM library, or manually in your sketch if it is required)
+GLCD glcd(serialLCD);
+
+// Create variables that will be editable through the menu and assign them initial values
+int number = -512;
+boolean enablePrint = false;
+
+// Create two menu item objects of class GEMItem, linked to number and enablePrint variables 
+GEMItem menuItemInt("Number:", number);
+GEMItem menuItemBool("Enable print:", enablePrint);
+
+// Create menu button that will trigger printData() function. It will print value of our number variable
+// to Serial monitor if enablePrint is true. We will write (define) this function later. However we should
+// forward-declare it in order to pass its reference to GEMItem constructor
+void printData(); //Forward declaration
+GEMItem menuItemButton("Print", printData);
+
+// Create menu page object of class GEMPage. Menu page holds menu items (GEMItem) and represents menu level.
+// Menu can have multiple menu pages (linked to each other) with multiple menu items each
+GEMPage menuPageMain("Main Menu");
+
+// Create menu object of class GEM. Supply its constructor with reference to glcd object we created earlier
+GEM menu(glcd);
+
+void setup() {
+  // Push buttons pin modes
+  pinMode(downPin, INPUT);
+  pinMode(leftPin, INPUT);
+  pinMode(rightPin, INPUT);
+  pinMode(upPin, INPUT);
+  pinMode(cancelPin, INPUT);
+  pinMode(okPin, INPUT);
+
+  // Serial communications setup
+  Serial.begin(115200);
+  serialLCD.begin(115200);
+
+  // LCD reset
+  delay(500);
+  glcd.reset();
+  delay(1000);
+  // Uncomment the following lines in dire situations
+  // (e.g. when screen becomes unresponsive after shutdown)
+  glcd.reset();
+  delay(1000);
+
+  // Menu init, setup and draw
+  menu.init();
+  setupMenu();
+  menu.drawMenu();
+}
+
+void setupMenu() {
+  // Add menu items to menu page
+  menuPageMain.addMenuItem(menuItemInt);
+  menuPageMain.addMenuItem(menuItemBool);
+  menuPageMain.addMenuItem(menuItemButton);
+
+  // Add menu page to menu and set it as current
+  menu.setMenuPageCurrent(menuPageMain);
+}
+
+void loop() {
+  // If menu is ready to accept button press...
+  if (menu.readyForKey()) {
+    // ...detect key press using KeyDetector library
+    myKeyDetector.detect();
+    // Pass pressed button to menu
+    // (pressed button ID is stored in trigger property of KeyDetector object)
+    menu.registerKeyPress(myKeyDetector.trigger);
+  }
+}
+
+void printData() {
+  // If enablePrint flag is set to true (checkbox on screen is checked)...
+  if (enablePrint) {
+    // ...print the number to Serial
+    Serial.print("Number is: ");
+    Serial.println(number);
+  } else {
+    Serial.println("Printing is disabled, sorry:(");
+  }
+}
