588711485263ad9cefd31ac8f2a5446868078e67
Daniel Wagner <dtw@dropbox.com>
2021-10-09 22:12:44 -0400

    allow exclusion of libraries using defines

--- src/config.h
+++ src/config.h
@@ -1,4 +1,15 @@
+#ifndef GEM_EXCLUDE_GLCD
 #include "config/enable-glcd.h"         // Enable AltSerialGraphicLCD version of GEM
+#endif
+
+#ifndef GEM_EXCLUDE_U8G2
 #include "config/enable-u8g2.h"         // Enable U8g2 version of GEM
+#endif
+
+#ifndef GEM_EXCLUDE_ADAFRUIT
 #include "config/enable-adafruit-gfx.h" // Enable Adafruit GFX version of GEM
+#endif
+
+#ifndef GEM_EXCLUDE_FLOAT_EDITS
 #include "config/support-float-edit.h"  // Support for editable float and double variables (option selects support them regardless of this setting)
+#endif
