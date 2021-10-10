ac6459674b97eda4034c8df25d90847a864d2b84
Alexander Spiridonov <alex.spiridonov@castle.co>
2021-10-10 16:02:21 +0400

    Config conditional inclusion refactored

--- src/config.h
+++ src/config.h
@@ -1,15 +1,23 @@
-#ifndef GEM_EXCLUDE_GLCD
+// GLCD support enabled by default.
+// Can be disabled either by defining GEM_DISABLE_GLCD (via compiler flag or define) or manual edition here.
+#ifndef GEM_DISABLE_GLCD
 #include "config/enable-glcd.h"         // Enable AltSerialGraphicLCD version of GEM
 #endif
 
-#ifndef GEM_EXCLUDE_U8G2
+// U8g2 support enabled by default.
+// Can be disabled either by defining GEM_DISABLE_U8G2 (via compiler flag or define) or manual edition here.
+#ifndef GEM_DISABLE_U8G2
 #include "config/enable-u8g2.h"         // Enable U8g2 version of GEM
 #endif
 
-#ifndef GEM_EXCLUDE_ADAFRUIT
+// Adafruit GFX support enabled by default.
+// Can be disabled either by defining GEM_DISABLE_ADAFRUIT_GFX (via compiler flag or define) or manual edition here.
+#ifndef GEM_DISABLE_ADAFRUIT_GFX
 #include "config/enable-adafruit-gfx.h" // Enable Adafruit GFX version of GEM
 #endif
 
-#ifndef GEM_EXCLUDE_FLOAT_EDITS
+// Support for editable float variables enabled by default.
+// Can be disabled either by defining GEM_DISABLE_FLOAT_EDIT (via compiler flag or define) or manual edition here.
+#ifndef GEM_DISABLE_FLOAT_EDIT
 #include "config/support-float-edit.h"  // Support for editable float and double variables (option selects support them regardless of this setting)
 #endif
