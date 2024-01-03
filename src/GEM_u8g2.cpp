7d9e7fd6366710991dc32a2ec595d5616f765206
Alexander Spiridonov <alex.spiridonov@castle.co>
2024-01-03 22:46:30 +0400

    Date bump

--- src/GEM_u8g2.cpp
+++ src/GEM_u8g2.cpp
@@ -14,7 +14,7 @@
   For documentation visit:
   https://github.com/Spirik/GEM
 
-  Copyright (c) 2018-2023 Alexander 'Spirik' Spiridonov
+  Copyright (c) 2018-2024 Alexander 'Spirik' Spiridonov
 
   This file is part of GEM library.
 
